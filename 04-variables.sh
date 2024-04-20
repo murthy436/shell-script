a20f9b3ec0ff19229dd7046f0dcd63366a5c78df
murthy436 <ukmurthy436@gmail.com>
2024-04-20 14:39:26 +0530

    script-pratice

---
+++ 04-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Declaring variables
+Person1=$1
+Person2=$2
+
+#Referring varialbes here
+
+echo "$Person1:: Hi $Person2 How are you?"
+echo "$Person2:: I am fine $Person1"
+echo "$Person1:: Where are you Woking from?"
+echo "$Person2:: I am working at Kurnool $Person1"
