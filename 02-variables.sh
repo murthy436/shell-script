89ef0ec03f9816819937eef12c5d44aff6cb5100
murthy436 <ukmurthy436@gmail.com>
2024-04-20 13:25:49 +0530

    some-message

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Yuvan:: Hello Sushanth where are you now?"
+echo "Sushanth:: Hi Yuvan, I am in Adoni. where are you?"
+echo "Yuvan: I am in Kurnool,we will meet tommorow is it possible to meet you?"
+echo "Sushanth:: Ya sure we catch up tommorow bro bye"
