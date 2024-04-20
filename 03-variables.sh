ed4e3edb06e67191dec17b8e3b3589765277ca8c
murthy436 <ukmurthy436@gmail.com>
2024-04-20 14:16:56 +0530

    script-pratice

--- 03-variables.sh
+++ 03-variables.sh
@@ -4,5 +4,5 @@ Person1=swathi
 Person2=krishna
 
 #refering variables
-echo "Person1:: Hi Person2 How are You?"
-echo "Person2:: I am fine how are you Person1"
+echo "$Person1:: Hi $Person2 How are You?"
+echo "$Person2:: I am fine how are you $Person1"
