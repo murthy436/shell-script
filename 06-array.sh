df8240f9f88359d4256a8a8cf89c381f30758eb7
murthy436 <ukmurthy436@gmail.com>
2024-04-22 19:13:42 +0530

    array-script-practice

--- 06-array.sh
+++ 06-array.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 Names=("Swathi" "Yuvan" "Sushanth")
-selected_index=3
+
 
 echo "First name: ${Names[0]}"
 echo "Second name is: ${Names[1]}"
 echo "list of all names is: ${Names[@]}"
 echo "list of full names is: ${Names[*]}"
+
+selected_index=3
 echo "Selected Index element at index: $selected_index:"
 echo "${Names[$selected_index]}"
