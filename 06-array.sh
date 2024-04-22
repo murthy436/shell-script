6a6337226c78875457d445dbfdea09c39427a604
murthy436 <ukmurthy436@gmail.com>
2024-04-22 18:36:50 +0530

    arrays-script-practice

--- 06-array.sh
+++ 06-array.sh
@@ -5,3 +5,4 @@ Names=("Swathi" "Yuvan" "Sushanth")
 echo "First name: ${Names[0]}"
 echo "Second name is: ${Names[1]}"
 echo "list of all names is: ${Names[@]}"
+echo "list of full names is: ${Names[*]}"
