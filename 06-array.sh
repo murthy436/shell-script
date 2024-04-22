881dba9808ef21db131053c3978db8e1b0bc758a
murthy436 <ukmurthy436@gmail.com>
2024-04-22 19:33:03 +0530

    array-script-practice

--- 06-array.sh
+++ 06-array.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-Names=("Swathi" "Yuvan" "Sushanth" "Naga rathna")
+Names=("Swathi" "Yuvan" "Sushanth" "Honey" "Naga rathna" "Govindu" "Chandra" "Devi")
 
 
 echo "First name: ${Names[0]}"
@@ -8,6 +8,10 @@ echo "Second name is: ${Names[1]}"
 echo "list of all names is: ${Names[@]}"
 echo "list of full names is: ${Names[*]}"
 
+#To print selected index element
 selected_index=3
 echo "Selected Index element at index: $selected_index:"
 echo "${Names[$selected_index]}"
+
+#To print elements from particular index
+echo "Elements from Particular index:"
