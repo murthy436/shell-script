64789cc60c850b52de93e438db93244a257f139a
murthy436 <ukmurthy436@gmail.com>
2024-04-24 13:49:46 +0530

    data-types-practice

--- 08-data-types1.sh
+++ 08-data-types1.sh
@@ -2,8 +2,8 @@
 
 #Reading values from user
 
-read -p "enter Num1: " Num1
-read -p "enter Num2: " Num2
+read -r -p "enter Num1: " Num1
+read -r -p "enter Num2: " Num2
 
 Sum=$(($Num1+$Num2))
 echo "Total sum of Num1 and Num2: "${Sum}
