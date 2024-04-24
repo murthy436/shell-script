cdfd2cd76321f96616d3a4d64d1cccfdbd2b35e2
murthy436 <ukmurthy436@gmail.com>
2024-04-24 13:35:48 +0530

    data-types-practice

--- 07-data-types.sh
+++ 07-data-types.sh
@@ -10,8 +10,8 @@ Num2=$2
 Num3=$3
 Num4=$4
 
-Sum=$(($Num1)+($Num2))
-Sub=$(($Num3)-($Num4))
+Sum=$(($Num1+$Num2))
+Sub=$(($Num3-$Num4))
 
 echo "Total of Num1 and Num2 is: $Sum"
 echo "Sub of Num3 and Num4 is: $Sub"
