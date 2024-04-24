39faee14069db065b9d641b6a9d6f387a20187b3
murthy436 <ukmurthy436@gmail.com>
2024-04-24 13:18:15 +0530

    data-types-practice

--- 07-data-types.sh
+++ 07-data-types.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-Num1=$1
-Num2=$2
-Num3=$3
-Num4=$4
+#Num1=$1
+#Num2=$2
+#Num3=$3
+#Num4=$4
 
-Sum=$(($Num1)+($Num2))
-Sub=$(($Num3)-($Num4))
+#Sum=$(($Num1)+($Num2))
+#Sub=$(($Num3)-($Num4))
 
-echo "Total of Num1 and Num2 is: $Sum"
-echo "Sub of Num3 and Num4 is: $Sub"
+#echo "Total of Num1 and Num2 is: $Sum"
+#echo "Sub of Num3 and Num4 is: $Sub"
+
+NO1=$1
+NO2=$2
+
+SUM=$(($NO1+$NO2))
+
+echo "Total of $NO1 and $NO2 is: $SUM"
