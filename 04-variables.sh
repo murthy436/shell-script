88798bf125aed84ded237a5c2dcbd325f37c9200
murthy436 <ukmurthy436@gmail.com>
2024-04-24 13:39:06 +0530

    data-types-practice

--- 04-variables.sh
+++ 04-variables.sh
@@ -3,6 +3,7 @@
 #Declaring variables
 Person1=$1
 Person2=$2
+Person3=$3
 
 #Referring varialbes here
 
@@ -10,3 +11,4 @@ echo "$Person1:: Hi $Person2 How are you?"
 echo "$Person2:: I am fine $Person1"
 echo "$Person1:: Where are you Woking from?"
 echo "$Person2:: I am working at Kurnool $Person1"
+echo "$Person3:: Hi guys how are you both of you"
