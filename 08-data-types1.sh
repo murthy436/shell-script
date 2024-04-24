7d73a006f48e92ffa425cf52cba99f5489afb2a1
murthy436 <ukmurthy436@gmail.com>
2024-04-24 16:14:51 +0530

    data-types-practice

--- 08-data-types1.sh
+++ 08-data-types1.sh
@@ -7,3 +7,12 @@ read -r -p "enter Num2: " Num2
 
 Sum=$(($Num1+$Num2))
 echo "Total sum of Num1 and Num2: "${Sum}
+
+Sub=$(($Num1-Num2))
+echo "Subtraction of Num1 and Num2: "${Sub}
+
+Mul=$(($Num1*Num2))
+echo "Multiplication of Num1 and Num2: "${Mul}
+
+Division=$(($Num1/$Num2))
+echo "Division of Num1 and Num2: "${Division}
