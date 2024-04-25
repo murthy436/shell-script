aee5fef21faf6f330a66f53cdf9caaa64815b08b
murthy436 <ukmurthy436@gmail.com>
2024-04-25 22:34:44 +0530

    Installation-packages

---
+++ 10-installation.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if($USERID -ne 0)
+    then
+        echo "Run this code with superuser"
+        exit 1
+    else
+        echo "You are super user"
+fi
+
+#Install mysql server
+dnf install mysql -y
+
+if($? -ne 0)
+    then
+        echo "Installing mysql server is failure"
+        exit 1
+    else
+        echo "Installation of mysql server is success"
+fi
+
+dnf install git -y
+
+if($? -ne 0)
+    then
+        echo "Installaton of git is failure"
+        exit 2
+    else
+        echo "Installation of git is Sucess"
+fi
+
+echo "is script is proceesiding"
+
+
+
