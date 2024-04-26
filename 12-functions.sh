f043ec17460d5019de97497c44425bdcf7a16eec
murthy436 <ukmurthy436@gmail.com>
2024-04-26 12:30:18 +0530

    Functions-practice

---
+++ 12-functions.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date + %f-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+VALIDATE()
+{
+    if [ $1 -ne 0]
+        then 
+            echo "$2 .....FAILURE"
+            exit1
+        else
+            echo "$2......SUCESS"
+
+    fi
+
+}
+
+if [ $USERID -ne 0 ]
+    then
+        echo "You need to run with super user"
+        exit 1
+    else
+        echo "You are super user"
+fi
+
+#Installing packages
+
+dnf install mysql -y &>>LOFFILE
+VALIDATE $? "Installing MYSQL"
+
+dnf install git -y &>>LOGFILE
+VALIDATE $? "Installing GIT"
