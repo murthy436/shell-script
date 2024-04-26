7f43d8b117a6cc6467e2e3a3a3498c146f4c77d5
murthy436 <ukmurthy436@gmail.com>
2024-04-26 12:34:51 +0530

    Functions-practice

--- 12-functions.sh
+++ 12-functions.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 USERID=$(id -u)
-TIMESTAMP=$(date + %f-%H-%M-%S)
+TIMESTAMP=$(date + %F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 VALIDATE()
 {
-    if [ $1 -ne 0]
+    if [ $1 -ne 0 ]
         then 
             echo "$2 .....FAILURE"
             exit1
